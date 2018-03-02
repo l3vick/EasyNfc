7e5988fde7e5007b6c8cc15fccb2f47158504250
Pablo <prc86@hotmail.com>
2018-03-02 14:06:00 +0100

    Custom security cypher spinner

---
+++ app/src/main/java/com/easynfc/writer/wi_fi/SpinnerAdapter.java
@@ -0,0 +1,75 @@
+package com.easynfc.writer.wi_fi;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.easynfc.R;
+import com.easynfc.util.AppUtils;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by pablorojas on 2/3/18.
+ */
+
+public class SpinnerAdapter extends ArrayAdapter<String> {
+
+    private String title;
+
+    public SpinnerAdapter(Context context, int resourceId, List<String> securityCypherList, String title) {
+        super(context, resourceId, securityCypherList);
+        this.title = title;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+
+
+
+        if (position == 0) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LinearLayout v = (LinearLayout) inflater.inflate(R.layout.spinner_header, parent, false);
+            TextView tv =  v.findViewById(R.id.dropdown_item);
+            tv.setTypeface(AppUtils.getAppTypeface(getContext()));
+            tv.setText(title);
+            return v;
+
+        } else {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            TextView v = (TextView) inflater.inflate(R.layout.spinner_item, parent, false);
+            TextView tv = v.findViewById(R.id.spinner_dialog_root);
+            tv.setText(getItem(position));
+            tv.setTypeface(AppUtils.getAppTypeface(getContext()));
+            tv.setHeight((int) (tv.getTextSize() * 2));
+            return v;
+        }
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount() + 1;
+    }
+
+    @Override
+    public String getItem(int position) {
+        if (position == 0) {
+            return title;
+        }
+        return super.getItem(position - 1);
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return position != 0;
+    }
+}
--- app/src/main/java/com/easynfc/writer/wi_fi/WiFiWriterFragment.java
+++ app/src/main/java/com/easynfc/writer/wi_fi/WiFiWriterFragment.java
@@ -1,16 +1,28 @@
 package com.easynfc.writer.wi_fi;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.easynfc.R;
 import com.easynfc.writer.BaseTypeFragment;
 import com.easynfc.writer.sms.SmsWriterContract;
 import com.easynfc.writer.sms.SmsWriterFragment;
+import com.easynfc.writer.url.UrlWriterContract;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +32,18 @@ public class WiFiWriterFragment extends BaseTypeFragment implements WiFiWriterCo
 
     public static final String TAG = "WiFiWriterFragment";
 
+    @BindView(R.id.txt_title)
+    TextView txtTitle;
+    @BindView(R.id.txt_wifi_password_title)
+    TextView txtWifiPasswordTitle;
+    @BindView(R.id.et_wifi_password)
+    EditText etWifiPassword;
+    @BindView(R.id.txt_wifi_ssid_title)
+    TextView txtWifiSsidTitle;
+    @BindView(R.id.et_wifi_ssid)
+    EditText etWifiSsid;
+    @BindView(R.id.sp_security_cypher)
+    Spinner spSecurityCypher;
     public WiFiWriterContract.Presenter presenter;
 
     public WiFiWriterFragment() {
@@ -34,8 +58,31 @@ public class WiFiWriterFragment extends BaseTypeFragment implements WiFiWriterCo
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.fragment_writer_wi_fi, container, false);
+        View v = inflater.inflate(R.layout.fragment_writer_wi_fi, container, false);
+        ButterKnife.bind(this, v);
+        setParentView(v);
+        List<String> securityCypherList = new ArrayList<>();
+        securityCypherList.add("Open");
+        securityCypherList.add("WEP");
+        securityCypherList.add("WPA PSK");
+        securityCypherList.add("WPA EAP");
+        securityCypherList.add("WPA2 EAP");
+        securityCypherList.add("WPA2 PSK");
+        SpinnerAdapter adapter = new SpinnerAdapter(getActivity(), R.layout.spinner_item, securityCypherList, "Select Security cypher");
+        spSecurityCypher.setAdapter(adapter);
         return v;
+    }
+
+
+    @OnClick(R.id.btn_record)
+    void onRecordTagBtnPressed() {
+        presenter.enableForegroundDispatch();
+        super.showDialog();
+    }
+
+    @OnClick(R.id.btn_save)
+    void onSaveTagBtnPressed() {
+
     }
 
     @Override
@@ -47,4 +94,17 @@ public class WiFiWriterFragment extends BaseTypeFragment implements WiFiWriterCo
     public void OnTagWritten() {
         super.tagWritten();
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        presenter.disableForegroundDispatch();
+    }
+
+    @Override
+    public void processNfc(Intent intent) {
+        super.processNfc(intent);
+        //presenter.writeTag(intent, etUrl.getText().toString());
+    }
+
 }
